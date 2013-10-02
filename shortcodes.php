008de3d6cc64ff24f3768f9c4685be8b208b88ac
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2013-10-02 18:26:03 -0400

    Added css and 2 more shortcodes
    Enqueue css for proper theme encapsulation and added shortcodes to test
    parameter taking and taking attributes to the short tag.
    
    This is damn easy.

--- shortcodes.php
+++ shortcodes.php
@@ -5,17 +5,32 @@
  */
 /*
 Plugin Name: DealCodes
-Plugin URI: git@git.bear-code.com:localvore
+Plugin URI: git@github.com:EJEHardenberg/dealcodes.git
 Description: A simple test plugin to work with shortcodes
 Author: Ethan J. Eldridge
 Version: 0.1
 Author URI: http://ejehardenberg.github.io
 */
 
+wp_enqueue_style( 'dealcodes_css', plugin_dir_url( __FILE__ ) . 'css/dealcodes.css');
 
+/* Simple function with no params */
 function signOffText() {  
-    return 'Thank you so much for reading! And remember to subscribe to our RSS feed. ';  
+    return 'This text brought to you by the shortcode signoff';  
 }  
 add_shortcode('signoff', 'signOffText');  
+
+function wrap($atts, $content = null){
+	return '<div class="bc">'.$content.'</div>';
+}
+add_shortcode('wrap', 'wrap');
+
+function wrapclass($atts, $content = null){
+	extract(shortcode_atts(array(  
+		"class" => ''  
+    ), $atts));  
+	return "<div class=\"bc $class\">".$content.'</div>';
+}
+add_shortcode('wrapclass','wrapclass');
 
 ?>
